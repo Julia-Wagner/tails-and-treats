1838d44d9dac8c4f89b75c1a600adbded1ed5ce4
julia Wager <julia@p-wagner.at>
2023-09-13 20:51:09 +0200

    feat: add modal to start the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 const RULES_MODAL = document.getElementById("modal-rules");
 const RULES_MODAL_BTN = document.getElementById("btn-rules");
 const RULES_MODAL_CLOSE = document.getElementById("close-rules");
+const START_GAME_MODAL = document.getElementById("modal-start-game");
+const START_GAME_MODAL_BTN = document.getElementById("btn-start-game");
+const START_GAME_MODAL_CLOSE = document.getElementById("close-start-game");
+const START_GAME_FORM = document.getElementById("start-game-form");
 const MAIN = document.getElementsByTagName("main")[0];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -14,6 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
             case RULES_MODAL_BTN:
                 RULES_MODAL.style.display = "block";
                 RULES_MODAL.setAttribute('aria-hidden', 'false');
+                break;
+            case START_GAME_MODAL_BTN:
+                START_GAME_MODAL.style.display = "block";
+                START_GAME_MODAL.setAttribute('aria-hidden', 'false');
                 break;
             default:
                 break;
@@ -29,12 +37,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 RULES_MODAL.style.display = "none";
                 RULES_MODAL.setAttribute('aria-hidden', 'true');
                 break;
+            case START_GAME_MODAL_CLOSE:
+                START_GAME_MODAL.style.display = "none";
+                START_GAME_MODAL.setAttribute('aria-hidden', 'true');
+                break;
             default:
                 break;
         }
     }
 
+    // start the game with the selected options from the form
+    function startGame(e) {
+        e.preventDefault();
+        console.log(START_GAME_FORM.dog.value);
+        console.log(START_GAME_FORM.difficulty.value);
+    }
+
     // event listeners
     RULES_MODAL_BTN.addEventListener("click", openModal);
     RULES_MODAL_CLOSE.addEventListener("click", closeModal);
+    START_GAME_MODAL_BTN.addEventListener("click", openModal);
+    START_GAME_MODAL_CLOSE.addEventListener("click", closeModal);
+    START_GAME_FORM.addEventListener("submit", startGame);
 });
