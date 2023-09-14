af77650abd0c5f8d77367e24bc7575574b903374
julia Wager <julia@p-wagner.at>
2023-09-14 11:16:55 +0200

    feat: hide main menu and show menu button when game is started

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ const START_GAME_MODAL_BTN = document.getElementById("btn-start-game");
 const START_GAME_MODAL_CLOSE = document.getElementById("close-start-game");
 const START_GAME_FORM = document.getElementById("start-game-form");
 const MAIN = document.getElementsByTagName("main")[0];
+const MENU_CONTAINER = document.getElementById("menu-container");
+const MENU_BTN = document.getElementById("game-menu");
 
 document.addEventListener("DOMContentLoaded", function () {
     // open the modal given as a parameter
@@ -15,10 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
         MAIN.setAttribute('aria-hidden', 'true');
         // open the model according to the clicked button
         switch (e.target) {
+            // open rules modal
             case RULES_MODAL_BTN:
                 RULES_MODAL.style.display = "block";
                 RULES_MODAL.setAttribute('aria-hidden', 'false');
                 break;
+            // open start game modal
             case START_GAME_MODAL_BTN:
                 START_GAME_MODAL.style.display = "block";
                 START_GAME_MODAL.setAttribute('aria-hidden', 'false');
@@ -33,11 +37,18 @@ document.addEventListener("DOMContentLoaded", function () {
         MAIN.setAttribute('aria-hidden', 'false');
         // close the model according to the clicked button
         switch (e.target) {
+            // close rules modal
             case RULES_MODAL_CLOSE:
                 RULES_MODAL.style.display = "none";
                 RULES_MODAL.setAttribute('aria-hidden', 'true');
                 break;
+            // close start game modal
             case START_GAME_MODAL_CLOSE:
+                START_GAME_MODAL.style.display = "none";
+                START_GAME_MODAL.setAttribute('aria-hidden', 'true');
+                break;
+            // game started - close modal
+            case START_GAME_FORM:
                 START_GAME_MODAL.style.display = "none";
                 START_GAME_MODAL.setAttribute('aria-hidden', 'true');
                 break;
@@ -49,8 +60,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // start the game with the selected options from the form
     function startGame(e) {
         e.preventDefault();
-        console.log(START_GAME_FORM.dog.value);
-        console.log(START_GAME_FORM.difficulty.value);
+        closeModal(e);
+        // hide main menu and show menu button
+        MENU_CONTAINER.style.display = "none";
+        MENU_CONTAINER.setAttribute('aria-hidden', 'true');
+        MENU_BTN.style.display = "flex";
+        MENU_BTN.setAttribute('aria-hidden', 'false');
     }
 
     // event listeners
